ae8343c8516a458d4d2a199dcc12d169ab35755f
antoroute <antoninherve92@gmail.com>
2025-04-21 16:49:25 +0200

    fastify.decorate

--- backend/messaging/index.js
+++ backend/messaging/index.js
@@ -16,6 +16,13 @@ await fastify.register(helmet);
 await fastify.register(fastifyJWT, {
   secret: process.env.JWT_SECRET
 });
+fastify.decorate("authenticate", async function (request, reply) {
+  try {
+    await request.jwtVerify();
+  } catch (err) {
+    reply.code(401).send({ error: "Unauthorized" });
+  }
+});
 await connectDB(fastify);
 
 groupRoutes(fastify);
